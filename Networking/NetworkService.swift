cd2da803d1a11510b29970d7b14c9b19edfb7328
Dmitry Ryzhkov <mation.me@icloud.com>
2023-06-10 18:06:43 +0200

    refactor NetworkService

--- Networking/NetworkService.swift
+++ Networking/NetworkService.swift
@@ -7,64 +7,29 @@
 
 import Foundation
 import Alamofire
-import SocketIO
 
 class NetworkService {
     static let shared = NetworkService()
 
-    private let baseURL: String = "http://localhost:53301/api" // замените на ваш адрес сервера
-
     private init() {}
 
     func getRequest(_ path: String, completion: @escaping (Result<Data, Error>) -> Void) {
-        let url = "\(baseURL)/\(path)"
-
-        AF.request(url).validate().responseData { response in
-            switch response.result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+        let data: [String: Any] = ["path": path]
+        WebSocketService.shared.sendDataToServer(eventName: "get", data: data)
     }
 
     func postRequest(_ path: String, parameters: Parameters, completion: @escaping (Result<Data, Error>) -> Void) {
-        let url = "\(baseURL)/\(path)"
-
-        AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default).validate().responseData { response in
-            switch response.result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+        let data: [String: Any] = ["path": path, "parameters": parameters]
+        WebSocketService.shared.sendDataToServer(eventName: "post", data: data)
     }
 
     func putRequest(_ path: String, parameters: Parameters, completion: @escaping (Result<Data, Error>) -> Void) {
-        let url = "\(baseURL)/\(path)"
-
-        AF.request(url, method: .put, parameters: parameters, encoding: JSONEncoding.default).validate().responseData { response in
-            switch response.result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+        let data: [String: Any] = ["path": path, "parameters": parameters]
+        WebSocketService.shared.sendDataToServer(eventName: "put", data: data)
     }
 
     func deleteRequest(_ path: String, completion: @escaping (Result<Data, Error>) -> Void) {
-        let url = "\(baseURL)/\(path)"
-
-        AF.request(url, method: .delete, encoding: JSONEncoding.default).validate().responseData { response in
-            switch response.result {
-            case .success(let data):
-                completion(.success(data))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
+        let data: [String: Any] = ["path": path]
+        WebSocketService.shared.sendDataToServer(eventName: "delete", data: data)
     }
 }
